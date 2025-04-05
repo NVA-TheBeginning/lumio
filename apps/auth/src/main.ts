00cb3622e6908d5a720b2c315bc971107a62bd05
Antony David <antonydavid945@gmail.com>
2025-04-05 08:06:53 +0200

    refactor(frontend): login cookie creation logic (#15)
    Create the cookie on Next.js instead of the API to simplify logic.
    Cookie are created on server side for better security.

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -5,12 +5,19 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "@/app.module.js";
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
 
   const config = new DocumentBuilder().setTitle("API").setVersion("1.0").build();
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, documentFactory);
+
+  app.enableCors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  });
+
+  Logger.log("CORS enabled with origin: * and methods: GET, POST, PUT, DELETE, OPTIONS");
 
   await app.listen(process.env.PORT ?? 3000, "0.0.0.0");
   const url = await app.getUrl();
--- apps/frontend/app/register/action.ts
+++ apps/frontend/app/register/action.ts
@@ -3,7 +3,7 @@ import type { RegisterFormValues } from "@/components/register-form";
 
 export async function registerApi(userData: RegisterFormValues): Promise<void> {
   const API_URL = "http://localhost:3000";
-  const response = await fetch(`${API_URL}/auth/register`, {
+  const response = await fetch(`${API_URL}/auth/signup`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
