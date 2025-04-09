3e0b800d34606744033747d2c93011ac310f074e
Antony David <antonydavid945@gmail.com>
2025-04-09 20:16:36 +0200

    feat(frontend): middleware

--- apps/frontend/app/middleware.ts
+++ apps/frontend/app/middleware.ts
@@ -1,11 +1,27 @@
 import { cookies } from "next/headers";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import { refreshTokens } from "@/lib/cookie";
 
 export async function middleware(request: NextRequest) {
-  const user = (await cookies()).get("user")?.value;
+  const cookiesStore = await cookies();
+  const user = cookiesStore.get("user")?.value;
   if (!user) {
-    return NextResponse.redirect(new URL("/", request.url));
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  const accessToken = cookiesStore.get("accessToken")?.value;
+  const refreshToken = cookiesStore.get("refreshToken")?.value;
+  if (!accessToken || !refreshToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  if (refreshToken && !accessToken) {
+    try {
+      await refreshTokens();
+    } catch (error) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
   }
 
   return NextResponse.next();
