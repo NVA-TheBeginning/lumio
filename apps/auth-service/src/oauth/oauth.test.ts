630bea18ecdecc657e20bab8959092b5a171df09
Neven Le Gall <neven.legall@reseau.sncf.fr>
2025-04-09 14:30:27 +0200

    fix tests

--- apps/auth-service/src/oauth/oauth.test.ts
+++ apps/auth-service/src/oauth/oauth.test.ts
@@ -1,7 +1,7 @@
-import { afterAll, beforeAll, describe, expect, test, spyOn } from "bun:test";
+import { afterAll, beforeAll, describe, expect, spyOn, test } from "bun:test";
 import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
 import { Test } from "@nestjs/testing";
-import axios, { AxiosResponse, AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 import { AppModule } from "@/app.module.js";
 import { PrismaService } from "@/prisma.service.js";
 
@@ -11,33 +11,34 @@ describe("OAuth", () => {
   const dummyMicrosoftToken = "dummy-microsoft-token";
 
   beforeAll(async () => {
-    spyOn(axios, "get").mockImplementation(
-        (async (url: string, config?: AxiosRequestConfig<any>): Promise<AxiosResponse<any>> => {
-          if (url.startsWith("https://www.googleapis.com/oauth2/v3/tokeninfo")) {
-            const queryString = url.split("?")[1] || "";
-            const queryParams = new URLSearchParams(queryString);
-            if (queryParams.get("id_token") === dummyGoogleToken) {
-              return Promise.resolve({ data: { email: "google.user@example.com" } } as AxiosResponse<any>);
-            } else {
-              return Promise.reject(new Error("Invalid Google token"));
-            }
-          }
+    spyOn(axios, "get").mockImplementation((async (
+      url: string,
+      config?: AxiosRequestConfig<any>,
+    ): Promise<AxiosResponse<any>> => {
+      if (url.startsWith("https://www.googleapis.com/oauth2/v3/tokeninfo")) {
+        const queryString = url.split("?")[1] || "";
+        const queryParams = new URLSearchParams(queryString);
+        if (queryParams.get("id_token") === dummyGoogleToken) {
+          return Promise.resolve({ data: { email: "google.user@example.com" } } as AxiosResponse<any>);
+        } else {
+          return Promise.reject(new Error("Invalid Google token"));
+        }
+      }
 
-          if (url.startsWith("https://graph.microsoft.com/v1.0/me")) {
-            if (config && config.headers && config.headers.Authorization === `Bearer ${dummyMicrosoftToken}`) {
-              return Promise.resolve({
-                data: {
-                  mail: "microsoft.user@example.com",
-                  userPrincipalName: "microsoft.user@example.com",
-                },
-              } as AxiosResponse<any>);
-            } else {
-              return Promise.reject(new Error("Invalid Microsoft token"));
-            }
-          }
-          return Promise.reject(new Error("Unknown URL"));
-        }) as any
-  );
+      if (url.startsWith("https://graph.microsoft.com/v1.0/me")) {
+        if (config && config.headers && config.headers.Authorization === `Bearer ${dummyMicrosoftToken}`) {
+          return Promise.resolve({
+            data: {
+              mail: "microsoft.user@example.com",
+              userPrincipalName: "microsoft.user@example.com",
+            },
+          } as AxiosResponse<any>);
+        } else {
+          return Promise.reject(new Error("Invalid Microsoft token"));
+        }
+      }
+      return Promise.reject(new Error("Unknown URL"));
+    }) as any);
 
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
