718474f164db484eb0662d090342584ea66f1a86
Antony David <antonydavid945@gmail.com>
2025-04-19 00:27:28 +0200

    fix(auth-service): circular swagger types (#74)

--- apps/auth-service/src/users/dto/students.dto.ts
+++ apps/auth-service/src/users/dto/students.dto.ts
@@ -2,41 +2,41 @@ import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsNotEmpty, IsOptional, IsString, IsStrongPassword } from "class-validator";
 
 export class CreateStudentDto {
-  @ApiProperty({ description: "Student's last name" })
+  @ApiProperty({ description: "Student's last name", type: "string" })
   @IsNotEmpty()
   @IsString()
   lastname!: string;
 
-  @ApiProperty({ description: "Student's first name" })
+  @ApiProperty({ description: "Student's first name", type: "string" })
   @IsNotEmpty()
   @IsString()
   firstname!: string;
 
-  @ApiProperty({ description: "Student's email address" })
+  @ApiProperty({ description: "Student's email address", type: "string" })
   @IsNotEmpty()
   @IsEmail()
   email!: string;
 }
 
 export class UpdateStudentDto {
-  @ApiProperty({ description: "Student's last name", required: false })
+  @ApiProperty({ description: "Student's lastname", required: false, type: "string" })
   @IsOptional()
   @IsString()
   lastname?: string;
 
-  @ApiProperty({ description: "Student's first name", required: false })
+  @ApiProperty({ description: "Student's firstname", required: false, type: "string" })
   @IsOptional()
   @IsString()
   firstname?: string;
 
-  @ApiProperty({ description: "Student's email address", required: false })
+  @ApiProperty({ description: "Student's email address", required: false, type: "string" })
   @IsOptional()
   @IsEmail()
   email?: string;
 }
 
 export class UpdatePasswordDto {
-  @ApiProperty({ description: "New password for the student" })
+  @ApiProperty({ description: "New password for the student", type: "string" })
   @IsNotEmpty()
   @IsString()
   @IsStrongPassword({
