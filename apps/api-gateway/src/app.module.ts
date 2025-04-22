236a08f550386d49c4ee4f871caf15cbf469d6e9
Baguette <87232192+Baguette42@users.noreply.github.com>
2025-04-22 10:36:07 +0200

    Add MicroserviceProxyModule in app.module.ts

--- apps/api-gateway/src/app.module.ts
+++ apps/api-gateway/src/app.module.ts
@@ -3,10 +3,10 @@ import { ConfigModule } from "@nestjs/config";
 import { HealthController } from "@/health/health.controller.js";
 import { AuthModule } from "@/microservices/auth/auth.module.js";
 import { ProjectsModule } from "@/microservices/projects-groups/projects.module.js";
+import { MicroserviceProxyModule } from "@/proxies/microservice-proxy.module.js";
 import configuration from "./config/configuration.js";
 import { UsersModule } from "./microservices/auth/users/users.module.js";
 import { FilesModule } from "./microservices/files/files.module.js";
-import {MicroserviceProxyModule} from "@/proxies/microservice-proxy.module.js";
 
 @Module({
   imports: [
