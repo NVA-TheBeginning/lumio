3bcb87234f86bfbff47ca4cab21b0ac7e47156de
vanande <vkhatchatrian1@myges.fr>
2025-04-22 12:15:53 +0200

    feat(promotions): add totalPages and size

--- apps/auth-service/src/users/users.controller.ts
+++ apps/auth-service/src/users/users.controller.ts
@@ -54,12 +54,12 @@ export class UsersController {
   @ApiOperation({ summary: "Find multiple users by their IDs" })
   @ApiOkResponse({ description: "Users found successfully" })
   @ApiQuery({ name: "ids", type: [Number], description: "Array of user IDs to find", required: true })
-  async findUsers(@Query("ids") ids: string, @Query("page") page?: string, @Query("rowsPerPage") rowsPerPage?: string) {
+  async findUsers(@Query("ids") ids: string, @Query("page") page?: string, @Query("size") size?: string) {
     if (!ids) {
       return [];
     }
 
     const userIds = ids.split(",").map((id) => parseInt(id, 10));
-    return await this.usersService.findUsersByIds(userIds, page, rowsPerPage);
+    return await this.usersService.findUsersByIds(userIds, page, size);
   }
 }
