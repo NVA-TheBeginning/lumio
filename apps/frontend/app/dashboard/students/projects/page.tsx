fce7b65b76b4cc999d64ff50742831f10b8b951d
Antony David <antonydavid945@gmail.com>
2025-04-23 16:50:34 +0200

    feat(frontend): setup settings page for students

---
+++ apps/frontend/app/dashboard/students/projects/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProjetcsPage() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h1 className="text-3xl font-bold">Projets</h1>
+      </div>
+    </div>
+  );
+}
