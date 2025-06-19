5932c902fea703c16401a2a957d4dc46e3872db4
Antony David <antonydavid945@gmail.com>
2025-06-19 15:17:05 +0200

    fix: build

--- apps/api-gateway/src/microservices/projects-groups/promotions/promotions.controller.ts
+++ apps/api-gateway/src/microservices/projects-groups/promotions/promotions.controller.ts
@@ -6,6 +6,7 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  ParseBoolPipe,
   ParseIntPipe,
   Patch,
   Post,
--- apps/auth-service/src/users/users.controller.ts
+++ apps/auth-service/src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, ParseBoolPipe, ParseIntPipe, Patch, Post, Query } from "@nestjs/common";
 import { ApiBody, ApiCreatedResponse, ApiOkResponse, ApiOperation, ApiParam, ApiQuery, ApiTags } from "@nestjs/swagger";
 import { CreateStudentDto, UpdatePasswordDto, UpdateStudentDto } from "./dto/students.dto";
 import { UsersService } from "./users.service";
